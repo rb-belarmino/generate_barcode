24e564d48e8c85477d27cbfc3cd9eb739d384c33
Rodrigo Belarmino <rb.belarmino@gmail.com>
2024-02-08 16:17:49 -0300

    feat: add error treatment

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,10 +1,13 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controller.tag_reator_controller import TagCreatorController
 
 class TagCreatorView:
     def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
+        tag_creator_controller = TagCreatorController()
+
         body = http_request.body
         product_code = body['product_code']
+        formatted_response = tag_creator_controller.create(product_code)
 
-        print("Estou na view")
-        return HttpResponse(status_code=200, body={"Hello": "world"})
+        return HttpResponse(status_code=200, body=formatted_response)
