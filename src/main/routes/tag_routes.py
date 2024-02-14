83a290f880e5840432418aaab39ff73b431772b0
Rodrigo Belarmino <rb.belarmino@gmail.com>
2024-02-14 11:15:36 -0300

    feat: implement validator

--- src/main/routes/tag_routes.py
+++ src/main/routes/tag_routes.py
@@ -4,13 +4,16 @@ from src.views.tag_creator_view import TagCreatorView
 
 from src.errors.error_handler import handle_errors
 
+from src.validators.tag_creator_validator import tag_creator_validator
+
 tags_routes_bp = Blueprint('tags_routes', __name__)
 
 @tags_routes_bp.route('/create_tag', methods=['POST'])
 def create_tags():
     response = None
     try:
-    
+        
+        tag_creator_validator(request)
         tag_cretor_view = TagCreatorView()
         
         http_request = HttpRequest(body=request.json)
